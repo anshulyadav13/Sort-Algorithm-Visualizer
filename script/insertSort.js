e41f7cf9a6b38b90d2f48047591675387f5e0b85
Anshul Yadav <anyadav411998@gmail.com>
2022-12-25 22:46:38 +0530

    transition update in bars

--- script/insertSort.js
+++ script/insertSort.js
@@ -6,23 +6,43 @@ async function insertSort() {
 
     for (i = 1; i < len; i++) {
         let val = parseInt(child[i].innerHTML);
+        // let colorOfI=child[i].style.backgroundColor;
+        // child[i].style.backgroundColor="#fcbf1e";
+        await waitforme( speedRange  );
+        child[i].classList.add("bar-transition");
+
 
         j = i - 1;
 
         while (j >= 0 && parseInt(child[j].innerHTML) > val) {
             let heightAtJ = parseInt(child[j].innerHTML);
+            let colorOfJ=child[j].style.backgroundColor;
+            child[j].classList.add("bar-transition");
+            child[j].style.backgroundColor="#c0eee6";
             await waitforme(100 + (speedRange / 10));
+            let colorOfJ1=child[j+1].style.backgroundColor;
+            
+            // child[j+1].classList.add("bar-transition");
+            await waitforme( speedRange  );
+            child[j+1].style.backgroundColor="#c0eee6";
             child[j + 1].innerHTML = heightAtJ;
             child[j + 1].style.height = heightAtJ + "vh";
-
+            await waitforme( speedRange  );
+            child[j].classList.remove("bar-transition");
+            child[j].style.backgroundColor=colorOfJ;
+            await waitforme( speedRange );
+            // child[j+1].classList.remove("bar-transition");
+            child[j+1].style.backgroundColor=colorOfJ1;
+           
             j--;
         }
-        await waitforme(100 + (speedRange / 10));
+        await waitforme( speedRange  );
         child[j + 1].innerHTML = val;
         child[j + 1].style.height = val + "vh";
 
-        child[0].style.backgroundColor = "#fcbf1e";
+        // child[i-1].style.backgroundColor = "#fcbf1e";
     }
+    // child[child.length-1].style.backgroundColor = "#fcbf1e";
     enableControls();
 
     document.title = "Sorting Visulizer";
--- script/selectionSort.js
+++ script/selectionSort.js
@@ -6,16 +6,32 @@ async function selectionSort() {
 
     for (i = 0; i < len - 1; i++) {
         let min = i;
-
+        child[i].style.backgroundColor = "#c0eee6";
+        child[i].classList.add("bar-transition");
+        await waitforme(100 + (speedRange / 10));
         for (j = i + 1; j < len; j++) {
             const heightAtMin = parseInt(child[min].innerHTML);
+            let colorOfJ=child[j].style.backgroundColor;
+            child[j].style.backgroundColor = "#c0eee6";
+            await waitforme(100 + (speedRange / 10));
+
             const heightAtJ = parseInt(child[j].innerHTML);
             if (heightAtMin > heightAtJ) {
+               
                 min = j;
             }
+            child[j].style.backgroundColor = colorOfJ;
+            await waitforme(100 + (speedRange / 10));
         }
-        await swapBars(child, i, min);
-        child[0].style.backgroundColor = "#fcbf1e";
+        let colorOfMin=child[min].style.backgroundColor;
+        child[min].style.backgroundColor = "#fcbf1e";
+            await waitforme(100 + (speedRange / 10));
+            
+            await swapBars(child, i, min);
+            child[min].style.backgroundColor=colorOfMin;
+            await waitforme(100 + (speedRange / 10));
+        child[i].classList.remove("bar-transition");
+        child[i].style.backgroundColor = "#fcbf1e";
     }
     enableControls();
     document.title = "Sorting Visulizer";
